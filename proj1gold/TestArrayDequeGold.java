df0444548783237504adb01a9b9b01dee4c82f4d
sweetspot00 <1904696960@qq.com>
2021-03-13 20:54:19 +0800

    proj1gold change if condition

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -22,24 +22,25 @@ public class TestArrayDequeGold {
                 corr.addLast(number);
 //                message.append(String.format("addLast(%d)\n", i));
                 System.out.printf("addFirst(%d)\n", number);
-            } else if (numberBetweenZeroAndOne >= 0.5 && numberBetweenZeroAndOne < 0.75) {
-                if (my.size() == 0) {
-                    continue;
-                }
+            } else if (numberBetweenZeroAndOne >= 0.5 && numberBetweenZeroAndOne < 0.75
+                        && !my.isEmpty() && !corr.isEmpty()) {
                 Integer myx = my.removeLast();
                 Integer cx = corr.removeLast();
 //                message.append("removeLast()\n");
-                assertEquals(cx, myx);
                 System.out.print("removeLast()\n");
+                assertEquals(cx, myx);
 
-            } else {
+            } else if (numberBetweenZeroAndOne >= 0.75 && !my.isEmpty() && !corr.isEmpty()) {
                 if (my.size() == 0) {
                     continue;
                 }
                 Integer myx = my.removeFirst();
                 Integer cx = corr.removeFirst();
-//                assertEquals(message.toString(), cx, myx);
                 System.out.print("removeFirst()\n");
+                assertEquals(cx, myx);
+
+            }else {
+                assertEquals(my.isEmpty(),corr.isEmpty());
             }
 //            System.out.print("my:");
 //            my.printDeque();
