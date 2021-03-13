4d2f51a18b8dd359f6d7dea6af597f6a9279808b
sweetspot00 <1904696960@qq.com>
2021-03-13 17:45:04 +0800

    proj1gold v1

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+
+public class TestArrayDequeGold {
+
+    @Test
+    public void testDeque(){
+        StudentArrayDeque<Integer> my = new StudentArrayDeque<Integer>();
+        ArrayDequeSolution<Integer> corr = new ArrayDequeSolution<Integer>();
+        int i = 0;
+        StringBuilder message = new StringBuilder();
+        while (i < 10000) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            if (numberBetweenZeroAndOne<0.25) {
+                my.addFirst(i);
+                corr.addFirst(i);
+                message.append(String.format("addLast(%d)\n", i));
+            }
+            else if (numberBetweenZeroAndOne<0.5 && numberBetweenZeroAndOne>=0.25) {
+                my.addLast(i);
+                corr.addLast(i);
+                message.append(String.format("addFirst(%d)\n", i));
+            }
+            else if (numberBetweenZeroAndOne>=0.5 && numberBetweenZeroAndOne<0.75) {
+                if (my.size()==0) {
+                    continue;
+                }
+                Integer myx = my.removeLast();
+                Integer cx = corr.removeLast();
+                message.append("removeLast()\n");
+                assertEquals(message.toString(),cx, myx);
+            }
+            else {
+                if (my.size()==0) {
+                    continue;
+                }
+                Integer myx = my.removeFirst();
+                Integer cx = corr.removeFirst();
+                message.append("removeFirst()\n");
+                assertEquals(message.toString() , cx, myx);
+            }
+            i += 1;
+
+        }
+
+
+    }
+}
